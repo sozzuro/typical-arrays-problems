1fbb03ccf311329f65457afdc9d45ef2a9674a9a
Denis Radivil <sozzuro@gmail.com>
2020-09-29 19:40:08 +0300

    feat: complete typical-array-problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNumber = 0
+  
+  if (!Array.isArray(array) || array.length <= 0) {
+    return 0;
+  }
+  
+  array.map(item => item < minNumber ? minNumber = item : minNumber) 
+  return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNumber = 0
+  
+  if (!Array.isArray(array) || array.length <= 0) {
+    return 0;
+  }
+  
+  array.map(item => item > maxNumber ? maxNumber = item : maxNumber) 
+  return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length <= 0) {
+    return 0;
+  }
+
+  return array.reduce((prevValue, item) => item + prevValue) / array.length;
 }
